a6df3326b7b8213861cf868e713f57bb11ff33f6
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-20 23:44:41 -0400

    basic layout and clerk install

---
+++ clerk-auth-nextjs/src/app/components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div className='p-5 w-full bg-fuchsia-500 bg-opacity-50 flex justify-between'>
+            <div className=''>
+                Logo
+            </div>
+            <div className=''>
+                Another
+            </div>
+        </div>
+    )
+}
+
+export default Nav
