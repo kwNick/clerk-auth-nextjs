12824722f5f39b9135779a8a351bab94aac1cf2b
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-21 21:53:35 -0400

    basic clerk signin auth

--- clerk-auth-nextjs/src/app/components/Nav.tsx
+++ clerk-auth-nextjs/src/app/components/Nav.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Nav = () => {
@@ -5,6 +6,14 @@ const Nav = () => {
         <div className='p-5 w-full bg-fuchsia-500 bg-opacity-50 flex justify-between'>
             <div className=''>
                 Logo
+            </div>
+            <div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
             <div className=''>
                 Another
--- clerk-auth-nextjs/src/app/layout.tsx
+++ clerk-auth-nextjs/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Nav";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className + "w-full min-h-screen"}>
-        <Nav />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className + "w-full min-h-screen"}>
+          <Nav />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
